78cbe62926f2431473f52982b23708174ec85ea6
sauale <61204377+sauale@users.noreply.github.com>
2020-03-30 21:03:09 +0300

    Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class Login extends Component {
-    render(){
-  return (
-    <div>
-      <p>Log in
-      </p>
+export default class Login extends Component {
+  render() {
+    return (
+      <Route exact path="/login">
+        <div className="FormCenter">
+          <form className="FormFields" onSubmit={this.handleSubmit}>
+            <div className="FormField">
+              <label className="FormField__Label" htmlFor="email">
+                Email{" "}
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="FormField_Input"
+                placeholder="Enter your full email"
+                name="password"
+              />
+            </div>
 
-    </div>
-  );
-    }
+            <div className="FormField">
+              <label className="FormField__Label" htmlFor="password">
+                Password{" "}
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="FormField_Input"
+                placeholder="Enter your full password"
+                name="password"
+              />
+            </div>
+
+            <div className="FormField">
+              <button className="FormField__Button mr-20">Log in </button>
+              <a href="register" className="FormField--Link">
+                I'm not registered
+              </a>
+            </div>
+          </form>
+        </div>
+      </Route>
+    );
+  }
 }
-export default Login;
