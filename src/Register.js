e3e7ebe9ccb364fd03cdaeb4a85c817c3a4202f7
sauale <61204377+sauale@users.noreply.github.com>
2020-03-30 21:12:19 +0300

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default class Login extends Component {
+export default class Register extends Component {
   render() {
     return (
       <Route exact path="/register">
